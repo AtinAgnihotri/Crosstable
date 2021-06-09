33e0df636fee866d675e845ad3265d05eb4b194e
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-09 23:36:20 +0530

    Initial Commit

---
+++ Crosstable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crosstable
+//
+//  Created by Atin Agnihotri on 09/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crosstable/CrosstableApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrosstableApp.swift
+//  Crosstable
+//
+//  Created by Atin Agnihotri on 09/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CrosstableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
